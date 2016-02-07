98ada3e0cb640f1a93b223e96d63c868ddd82b38
dimitar popov <dimitarhpopov@gmail.com>
2016-02-07 14:29:39 +0200

    how it works

---
+++ LodgeAround.Web/scripts/layout.js
@@ -0,0 +1,12 @@
+﻿function showHowItWorks()
+{
+    $('#how_it_works').height($(window).height());
+   // $('#background').css("top", $(window).height());
+
+    $("#how_it_works").slideDown("slow");
+}
+
+function closeHowItWorks()
+{
+    $("#how_it_works").slideUp("slow");
+}
